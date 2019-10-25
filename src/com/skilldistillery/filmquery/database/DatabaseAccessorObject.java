a1cf23a55913ff177242de2293e6fee997a6098e
Jacob <jacobshorterivey@gmail.com>
2019-10-25 17:37:41 -0600

    swapped repos

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.filmquery.database;
+
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+
+  @Override
+  public Film findFilmById(int filmId) {
+    return null;
+  }
+
+//public Actor findActorById(int actorId){ }
+//public List<Actor> findActorsByFilmId(int filmId){ }
+}
